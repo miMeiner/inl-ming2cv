bdbd1cda787b581e076a8527d1eef384f4445b29
miMeiner <christian.meiner@medieinstitutet.se>
2021-12-02 09:59:57 +0100

    heck, nu då

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
+window.addEventListener('load', main);
 
+function main() {
+}
+
+function skillScroll() {
+    let element = document.getElementById('skillsets');
+    element.scrollIntoView({ behavior: "smooth" });
+}
+function projectScroll() {
+    let element = document.getElementById('projects');
+    element.scrollIntoView({ behavior: "smooth" });
+}
+function aboutScroll() {
+    let element = document.getElementById('about-me');
+    element.scrollIntoView({ behavior: "smooth" });
+}
+function contactScroll() {
+    let element = document.getElementById('contact');
+    element.scrollIntoView({ behavior: "smooth" });
+}
