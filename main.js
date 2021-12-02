46670a29f0d3499fe50aa0514fec85ce1053b5c4
MiMeiner <Christian.Meiner@medieinstitutet.se>
2021-12-02 20:04:19 +0100

    updated with aos library and some MQ changes

--- main.js
+++ main.js
@@ -24,3 +24,26 @@ function topScroll() {
     let element = document.getElementById('front-page');
     element.scrollIntoView({ behavior: "smooth" });
 }
+/* EVERYTHING BELOW THIS LINE IS PART OF THE AOS LIBRARY
+IMPORTED FROM https://github.com/michalsnik/aos
+------------------------------------------------------ */
+AOS.init();
+AOS.init({
+  disable: false, 
+  startEvent: 'DOMContentLoaded', 
+  initClassName: 'aos-init', 
+  animatedClassName: 'aos-animate',
+  useClassNames: false, 
+  disableMutationObserver: false, 
+  debounceDelay: 50,
+  throttleDelay: 99, 
+  
+  offset: 120,
+  delay: 0,
+  duration: 400,
+  easing: 'ease',
+  once: false,
+  mirror: false,
+  anchorPlacement: 'top-bottom',
+
+});
