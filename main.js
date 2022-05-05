7a7843e7216e8fedf08ec313fea93cdf893bce57
miMeiner <christian.meiner@medieinstitutet.se>
2022-05-05 21:43:31 +0200

    updated small changes

--- main.js
+++ main.js
@@ -3,23 +3,47 @@ window.addEventListener('load', main);
 function main() {
 }
 /* ALL OF THE FUNCTIONS BELOW ARE USED TO SCROLL TO THEIR RESPECTIVE PART OF THE SCREEN.
-HEY GET THE ELEMENT AND THEN SCROLL SAID ELEMENT INTO THE VIEWPORT. */
+THEY GET THE ELEMENT AND THEN SCROLL SAID ELEMENT INTO THE VIEWPORT. */
+
+function vaderRedirect() {
+    window.open("https://projektvader.netlify.app/", "_blank")
+}
+function inetRedirect() {
+    window.open("https://www.inet.se/", "_blank")
+}
+function discordRedirect() {
+    window.open("https://discord.com/", "_blank")
+}
+function boxesRedirect() {
+    window.open("https://github.com/miMeiner/LAB1", "_blank")
+}
+function nftHeavenRedirect() {
+    window.open("https://nftheaven.netlify.app/", "_blank")
+}
+function spotifyRedirect() {
+    window.open("https://www.spotify.com/se/", "_blank")
+}
+
 function skillScroll() {
     let element = document.getElementById('skillsets');
     element.scrollIntoView({ behavior: "smooth" });
 }
+
 function projectScroll() {
     let element = document.getElementById('projects');
     element.scrollIntoView({ behavior: "smooth" });
 }
+
 function aboutScroll() {
     let element = document.getElementById('about-me');
     element.scrollIntoView({ behavior: "smooth" });
 }
+
 function contactScroll() {
     let element = document.getElementById('contact');
     element.scrollIntoView({ behavior: "smooth" });
 }
+
 function topScroll() {
     let element = document.getElementById('front-page');
     element.scrollIntoView({ behavior: "smooth" });
